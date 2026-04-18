6c66b466b8d299c7c56af8a3de288ee8bf62113e
yusuf <yusuf.ali1031@gmail.com>
2026-04-18 15:07:11 +0200

    finished async lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here!
+async function getPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+
+    displayPosts(posts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+
+  posts.forEach(function(post) {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    ul.appendChild(li);
+  });
+}
+
+getPosts();
